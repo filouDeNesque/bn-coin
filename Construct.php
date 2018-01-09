d1bb774085103df7c01f3f1f2419a1a63f4dbe22
filouDeNesque <p.cuquemy@yahoo.com>
2018-01-09 20:28:41 +0100

    first commit

---
+++ Construct.php
@@ -0,0 +1,130 @@
+<?php 
+class Extracteur{
+
+//variables
+	private $page;
+	private $recherche;
+	private $px;
+	private $nbp;
+
+//constructeur
+
+
+ //function
+ 	public function param($recherche="velo",$page=0,$px=1){
+ 		$this->page =$page;
+		$this->recherche = $recherche;
+		$this->px = $px;
+ 	}
+ 	
+ 	public function nmbp() 
+ 	{
+ 	//variables
+ 		$recherche = $this->recherche;
+ 		$page = $this->page;
+
+ 		$file2 = file_get_contents("https://www.leboncoin.fr/annonces/offres/bourgogne/?th=1&o=$page&q=$recherche&it=1&sp=1");
+		
+	//cherche le nombre de page
+		$nombredepage = preg_match_all("#<span class=\"tabsSwitchNumbers small-hidden tiny-hidden\"> (.+[0-9])<\/#", $file2, $nmbpage);
+		$nmi = $nmbpage[1][0];
+		$trimp =preg_replace("#\s#", "", $nmi);//enleve les espaces
+		$np  = (int)$trimp;
+		$nbp = $np/35; 	
+	//nombre de page total
+		$this->nbp = $nbp;
+		return $nbp;
+ 	}
+
+ 	public function cherche()
+ 	{
+ 	$nbp = $this->nbp;
+ 	$recherche = $this->recherche;
+ 	$px = $this->px;
+ 	$page = $this->page;
+	
+	for($pp=0;$pp<$nbp;$pp++) 
+	{
+		$file = file_get_contents("https://www.leboncoin.fr/annonces/offres/bourgogne/?o=$page&q=$recherche&it=1&sp=1");
+		$page++;
+		$nombre = preg_match_all("#price\" content=\"(.+)\"#", $file, $matches);
+		$prix = $matches[1][1];
+		$prixxie = (int)$prix;
+		//echo "<br> prixxie :$prixxie";
+		set_time_limit(500); //repousse le temp de recherche
+
+		if ($prixxie < $px) 
+		{
+			$page++;
+			// echo "$page";
+		}
+		else{
+			$name = preg_match_all("#item_title\" itemprop=\"name\">\n( )+(.+)#", $file, $nom);
+			$adresse = preg_match_all("#<meta itemprop=\"address\" content=\"(.+)\"#", $file, $addresses);
+	
+			$category = preg_match_all("#itemprop=\"category\" content=\"(.+)\"#", $file, $categ);
+	
+			$lien = preg_match_all("#<a href=\"(\/\/www\.leboncoin\.fr\/(.+)\/[0-9]+\.htm\?ca=[0-9]+_[a-z])#", $file, $li);
+
+			$date = preg_match_all("#<p class=\"item_supp\" itemprop=\"availabilityStarts\" content=\"([0-9]+-[0-9]+-[0-9]+)\"#", $file, $dat);
+
+			$heure = preg_match_all("#<p class=\"item_supp\" itemprop=\"availabilityStarts\" content=\"([0-9]+-[0-9]+-[0-9]+)\">\n+.+\n+\D+(.+)#", $file, $hr);
+		
+			set_time_limit(500);
+
+			for($i=0;$i<35;$i++)
+			{
+				echo "i : $i 	";
+				$prix = $matches[1][$i];
+				$prixxie = (int)$prix;
+				if ($prixxie < 1) 
+				{
+					break;
+				}
+				else
+				{
+					$add = $addresses[1][$i];
+					$addep =$i+1;
+					$addi= $addresses[1][$addep];
+					$nomine = $nom[2][$i];
+					$cat = $categ[1][$i];
+					$l = $li[1][$i];
+					$d = $dat[1][$i];
+					$h = $hr[2][$i];
+					
+					echo "$nomine /b"; 
+					/*	printf("<li>Prix : $prix Euros<lu>
+					<li>Nom : $nomine</li>
+					<li>Ville : $add $addi</li>
+					<li>Category : $cat</li>
+					<li>Date : $d : $h</li>
+					<li>Lien : <a href = $l>Voire la page !</a></li>
+					<li>page : $page</li>
+					</lu></li> ");
+					echo "<br/>";
+
+					*/
+				}
+			}
+		}
+	}
+}
+
+//Getter
+
+ 	public function getPage(){
+ 		$page= $this->page;
+ 		return $page;
+ 	}
+
+ 	public function getRecherche(){
+ 		$recherche = $this->recherche;
+ 		return $recherche;
+ 	} 
+
+ 	public function getPx(){
+ 		$px = $this->px;
+ 		return $px;
+ 	}  
+}
+?>
---
+++ exec.php
@@ -0,0 +1,105 @@
+<?php 
+require 'Construct.php'
+ ?>
+ <!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
+		<title> Le B-C C R</title>
+	</head>
+
+	<body>
+		<div class="container-fluid">	
+			<div class="row">
+			
+        <nav class="col-sm-3 col-md-2 d-none d-sm-block bg-light sidebar">
+<!--           <ul class="nav nav-pills flex-column">
+	<div class="titre">
+			<h1>B-C c r</h1>
+			</div>	
+  <li class="nav-item">
+    <a class="nav-link active" href="#">Overview <span class="sr-only">(current)</span></a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Reports</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Analytics</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Export</a>
+  </li>
+</ul> -->
+<ul class="nav nav-pills flex-column">
+       <!--      <li class="nav-item">
+         <a class="nav-link" href="#">Nav item</a>
+       </li>
+       <li class="nav-item">
+         <a class="nav-link" href="#">Nav item again</a>
+       </li>
+       <li class="nav-item">
+         <a class="nav-link" href="#">One more nav</a>
+       </li>
+       <li class="nav-item">
+         <a class="nav-link" href="#">Another nav item</a>
+       </li>
+                 </ul>
+       
+                 <ul class="nav nav-pills flex-column">
+       <li class="nav-item">
+         <a class="nav-link" href="#">Nav item again</a>
+       </li>
+       <li class="nav-item">
+         <a class="nav-link" href="#">One more nav</a>
+       </li>
+       <li class="nav-item">
+         <a class="nav-link" href="#">Another nav item</a>
+       </li>
+                 </ul> -->
+        </nav>
+
+        <main role="main" class="col-sm-9 ml-sm-auto col-md-10 pt-3">
+          <h1>Stat BC</h1>
+
+          <section class="row text-center placeholders">
+            <div class="col-6 col-sm-3 placeholder">
+              <img src="data:image/gif;base64,R0lGODlhAQABAIABAAJ12AAAACwAAAAAAQABAAACAkQBADs=" class="img-fluid rounded-circle" alt="Generic placeholder thumbnail" width="200" height="200">
+              <h4>Pages</h4>
+              <div class="text-muted">Something else</div>
+            </div>
+            <div class="col-6 col-sm-3 placeholder">
+              <img src="data:image/gif;base64,R0lGODlhAQABAIABAADcgwAAACwAAAAAAQABAAACAkQBADs=" class="img-fluid rounded-circle" alt="Generic placeholder thumbnail" width="200" height="200">
+              <h4>Articles</h4>
+              <span class="text-muted">Something else</span>
+            </div>
+            <div class="col-6 col-sm-3 placeholder">
+              <img src="data:image/gif;base64,R0lGODlhAQABAIABAAJ12AAAACwAAAAAAQABAAACAkQBADs=" class="img-fluid rounded-circle" alt="Generic placeholder thumbnail" width="200" height="200">
+              <h4>Prix > 100</h4>
+              <span class="text-muted">Something else</span>
+            </div>
+            <div class="col-6 col-sm-3 placeholder">
+              <img src="data:image/gif;base64,R0lGODlhAQABAIABAADcgwAAACwAAAAAAQABAAACAkQBADs=" class="img-fluid rounded-circle" alt="Generic placeholder thumbnail" width="200" height="200">
+              <h4>prix < 100</h4>
+              <span class="text-muted">Something else</span>
+            </div>
+           </section>
+        </main>
+    </div>
+	</div>
+	<?php
+		$Construct  = new Extracteur();
+		$Construct->param("chameau",0,1);
+		$recherche = $Construct->getRecherche();
+		$recherche = $Construct->nmbp();
+		$extract = $Construct->cherche();
+		var_dump($Construct->nomine);
+
+
+	?>
+	 
+	</body>	
+</html>		
+
+
